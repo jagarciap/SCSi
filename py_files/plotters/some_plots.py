1a194e59b2faf561868506b9bafd796b506d4900
Jorge Garcia <jorge-02@hotmail.es>
2020-02-18 15:01:33 +0900

    Now some plots works. There is an issue with how I am loading the velocities of the particles

--- py_files/plotters/some_plots.py
+++ py_files/plotters/some_plots.py
@@ -4,13 +4,20 @@ import sys
 
 sys.path.insert(0,'..')
 
+import constants as c
+from mesh import Mesh_2D_rm
 import vtr_to_numpy as vtn
+
+mesh = Mesh_2D_rm(c.XMIN, c.XMAX, c.YMIN, c.YMAX, c.NX, c.NY, c.DEPTH)
 
 def mean_velx_time(name):
     fig = plt.figure(figsize=(8,8))
-    data = vtn.vtrToNumpy(vtn.loadFromResults(), name)[0]
-    arr = numpy.average(data, axis = (0,1))
-    fig.plot(arr)
+    data = vtn.vtrToNumpy(mesh, vtn.loadFromResults(), name)
+    arr_e = numpy.average(data[0], axis = 0)
+    arr_p = numpy.average(data[1], axis = 0)
+    plt.plot(arr_e, label = 'Electrons')
+    plt.plot(arr_p, label = 'Protons')
+    plt.legend()
     fig.show()
 
-mean_velx_time("Electron - Solar wind-temperature")
+mean_velx_time(["Electron - Solar wind-temperature", "Proton - Solar wind-temperature"])
--- py_files/vtr_to_numpy.py
+++ py_files/vtr_to_numpy.py
@@ -6,29 +6,27 @@ import sys
 from vtk.util.numpy_support import vtk_to_numpy
 from subprocess import check_output
 
-from mesh import Mesh_2D_rm as mesh
-
-def vtrToNumpy(filenames, names):
+def vtrToNumpy(mesh, filenames, names):
     arrays = []
     #First filename
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     cwd = os.path.split(os.getcwd())[0]
     filename = cwd+'/results/'+filenames[0]
-    reader = mesh.vtkReader(mesh)
+    reader = mesh.vtkReader()
     reader.SetFileName(filename)
     reader.Update()
     output = reader.GetOutput()
     for name in names:
-        arrays.append(mesh.reverseVTKOrdering(mesh,vtk_to_numpy(output.GetPointData().GetArray(self.name)))[:,None])
+        arrays.append(mesh.reverseVTKOrdering(vtk_to_numpy(output.GetPointData().GetArray(name)))[:,None])
     for filename in filenames[1:]:
         cwd = os.path.split(os.getcwd())[0]
         filename = cwd+'/results/'+filename
-        reader = mesh.vtkReader(mesh)
+        reader = mesh.vtkReader()
         reader.SetFileName(filename)
         reader.Update()
         output = reader.GetOutput()
         for i in range(len(names)):
-            arrays[i] = numpy.append(arrays[i],mesh.reverseVTKOrdering(mesh,vtk_to_numpy(output.GetPointData().GetArray(self.names[i]))), axis = 2)
+            arrays[i] = numpy.append(arrays[i],mesh.reverseVTKOrdering(vtk_to_numpy(output.GetPointData().GetArray(names[i])))[:,None], axis = arrays[i].ndim-1)
     return arrays
 
 def loadFromResults():
@@ -37,5 +35,5 @@ def loadFromResults():
     cwd = cwd+'/results/'
     stdout = check_output('ls' +' {}'.format(cwd), shell=True)
     files = stdout.decode().split(sep='\n')
-    return files
+    return files[:-1]
 
